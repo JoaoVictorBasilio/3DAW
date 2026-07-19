18f475ca52d492ac43079faf7a5758e678c2f296
João Victor <joaovictorbasilio01@gmail.com>
2026-07-19 16:23:55 -0300

    Delete AV2 directory

--- AV2/php/auth/sessao.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* AV2 file: Endpoint que verifica se o usuário está logado no momento. */
-if (isset($_SESSION['user_id'])) {
-    echo json_encode(['logado' => true, 'nome' => $_SESSION['user_nome'], 'is_admin' => (bool)($_SESSION['is_admin'] ?? false)]);
-} else {
-    echo json_encode(['logado' => false]);
-}
-?>
